e6358b8abd7bb25f94864f3db0ff7c73ce64e5cb
Jonathan Frawley <jonathan.frawley@durham.ac.uk>
2021-04-30 17:09:30 +0100

    Clean up gen_angles script

--- preprocessing/gen_angles.py
+++ preprocessing/gen_angles.py
@@ -27,76 +27,15 @@ for i in range(216):
     arrs.append(arr)
 
 arr = np.array(arrs)
-#print(arr.shape)
 
-#fig, axs = plt.subplots(3)
-
-# Axis projections
 os.makedirs('out', exist_ok=True)
-'''
-for axis in range(3):
-    max_im = np.max(arr, axis=axis)
-    n = normalize(max_im)
-    n = resize(n, (128, 128))
-    n[n<0.3] = 0
-    skimage.io.imsave(f'out/{axis}.png', ((1.0 - n) * 255).astype(np.uint8))
-    skimage.io.imsave(f'out/{axis}_normalize.png', ((1.0 - n) * 255).astype(np.uint8))
-    #print(max_im.shape)
-    #axs[axis].imshow(max_im)
-'''
-#plt.show()
 
 # Angles
-'''
-print(arr.shape)
-
-fig, axs = plt.subplots(3)
-for axis_1, axis_2 in [[0, 1], [0, 2], [1,0], [1,2], [2, 0], [2,1]]:
-    for axis, angle in enumerate([60, 90, 120]):
-        #angle = math.pi / 6  # 30 deg
-        #angle = math.pi / 2  # 30 deg
-        #rotated = scipy.ndimage.interpolation.rotate(arr, angle, axes=(1, 2))
-        #rotated = scipy.ndimage.interpolation.rotate(arr, angle, axes=(0, 1))
-        #rotated = scipy.ndimage.interpolation.rotate(arr, angle, axes=(0, 2))
-        rotated = scipy.ndimage.interpolation.rotate(arr, angle, axes=(axis_1, axis_2))
-
-        print(rotated.shape)
-        max_im = np.max(rotated, axis=0)
-        #axs[axis].imshow(max_im, cmap="gray")
-
-        max_im = normalize(max_im)
-        max_im = resize(max_im, (128, 128))
-        skimage.io.imsave(f'out/{axis_1}_{axis_2}_{axis}.png', ((1.0 - max_im) * 255).astype(np.uint8))
-    #plt.show()
-        #max_im = np.max(arr, axis=axis)
-        #n = normalize(max_im)
-        #n = resize(n, (128, 128))
-        #n[n<0.3] = 0
-        #skimage.io.imsave(f'out/{axis}.png', ((1.0 - n) * 255).astype(np.uint8))
-        #skimage.io.imsave(f'out/{axis}_normalize.png', ((1.0 - n) * 255).astype(np.uint8))
-        ##print(max_im.shape)
-        ##axs[axis].imshow(max_im)
-'''
-
-print(arr.shape)
-
-fig, axs = plt.subplots(5)
 angle = 90
 axis = 0
 axis_1 = 1
 axis_2 = 0
 rotated = scipy.ndimage.interpolation.rotate(arr, angle, axes=(axis_1, axis_2))
-print(rotated.shape)
-max_im = np.max(rotated, axis=0)
-axs[axis].imshow(max_im, cmap="gray")
-
-#axis_1 = 1
-#axis_2 = 0
-
-#axis_1 = 1
-#axis_2 = 2
-#axis_1 = 2
-#axis_2 = 1
 
 axis_1 = 2
 axis_2 = 0
@@ -113,38 +52,3 @@ while angle < 360:
 
     angle += angle_delta
     idx += 1
-exit()
-
-angle = 60
-axis = 1
-axis_1 = 2
-axis_2 = 0
-new = scipy.ndimage.interpolation.rotate(rotated, angle, axes=(axis_1, axis_2))
-
-max_im = np.max(new, axis=0)
-axs[axis].imshow(max_im, cmap="gray")
-
-angle = 30
-axis = 2
-axis_1 = 2
-axis_2 = 0
-new = scipy.ndimage.interpolation.rotate(rotated, angle, axes=(axis_1, axis_2))
-
-max_im = np.max(new, axis=0)
-axs[axis].imshow(max_im, cmap="gray")
-
-angle = 300 
-axis = 3
-new = scipy.ndimage.interpolation.rotate(rotated, angle, axes=(axis_1, axis_2))
-
-max_im = np.max(new, axis=0)
-axs[axis].imshow(max_im, cmap="gray")
-
-angle = 330 
-axis = 4
-new = scipy.ndimage.interpolation.rotate(rotated, angle, axes=(axis_1, axis_2))
-
-max_im = np.max(new, axis=0)
-axs[axis].imshow(max_im, cmap="gray")
-
-plt.show()
