73c421149e64718ddfc15d1f6de3742dc87ae5d6
Jonathan Frawley <jonathan.frawley@durham.ac.uk>
2021-06-03 17:22:06 +0100

    Finish prepping CT images for CycleGAN

--- scripts/construct_cyclegan_dataset.py
+++ scripts/construct_cyclegan_dataset.py
@@ -1,5 +1,6 @@
 import os
 
+from pydicom import dcmread
 import numpy as np
 import skimage.io
 from skimage.transform import resize
@@ -14,9 +15,13 @@ def normalize(arr):
 
 
 CHEXPERT_DIR = '/home/jmf/phd/datasets/data/chexpert/CheXpert-v1.0-small'
-N_TRAIN = 1096
-N_VAL = 1098
-N_TEST = 1098
+RICORD_DIR = '/mnt/data1/data/ricord/manifest-1612365584013'
+#N_TRAIN = 1096
+#N_VAL = 1098
+#N_TEST = 1098
+N_TRAIN = 80
+N_VAL = 10
+N_TEST = 10
 #OUTPUT_DIM = (600, 600)
 OUTPUT_DIM = (300, 300)
 OUTPUT_PREFIX = 'mednerf_xray_to_ct_dataset'
@@ -29,14 +34,12 @@ OUTPUT_B_TEST = f'{OUTPUT_PREFIX}/testB'
 
 
 # X-Ray
-def xray()
+def xray():
     os.makedirs(OUTPUT_A_TRAIN, exist_ok=True)
     for i in range(N_TRAIN):
         patient_n = f'{(i + 1):05}'
         chexpert_patient_dir = f'{CHEXPERT_DIR}/train/patient{patient_n}/study1'
-        print(f'Orig image: {chexpert_patient_dir}/view1_frontal.jpg')
         img = skimage.io.imread(f'{chexpert_patient_dir}/view1_frontal.jpg')
-        print(f'size: {img.shape} min {img.min()} max {img.max()}')
         img = normalize(img)
         img = resize(img, OUTPUT_DIM)
         skimage.io.imsave(f'{OUTPUT_A_TRAIN}/{i:04}.png', (img * 255).astype(np.uint8))
@@ -48,12 +51,10 @@ def xray()
         chexpert_patient_dir = f'{CHEXPERT_DIR}/train/patient{patient_n}/study1'
         fname = f'{chexpert_patient_dir}/view1_frontal.jpg'
 
-        print(f'Orig image: {fname}')
         if not os.path.exists(fname):
             print(f'Skipping {fname} as it does not exist')
             continue
         img = skimage.io.imread(f'{fname}')
-        print(f'size: {img.shape} min {img.min()} max {img.max()}')
         img = normalize(img)
         img = resize(img, OUTPUT_DIM)
         skimage.io.imsave(f'{OUTPUT_A_VAL}/{i:04}.png', (img * 255).astype(np.uint8))
@@ -66,13 +67,11 @@ def xray()
         chexpert_patient_dir = f'{CHEXPERT_DIR}/train/patient{patient_n}/study1'
         fname = f'{chexpert_patient_dir}/view1_frontal.jpg'
 
-        print(f'Orig image: {fname}')
         if not os.path.exists(fname):
             print(f'Skipping {fname} as it does not exist')
             in_idx += 1
             continue
         img = skimage.io.imread(f'{fname}')
-        print(f'size: {img.shape} min {img.min()} max {img.max()}')
         img = normalize(img)
         img = resize(img, OUTPUT_DIM)
         skimage.io.imsave(f'{OUTPUT_A_TEST}/{idx:04}.png', (img * 255).astype(np.uint8))
@@ -80,8 +79,42 @@ def xray()
         in_idx += 1
 
 # CT
+def process_one_ct(input_dir, output_dir, idx):
+    dicoms = []
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if file.endswith(".dcm"):
+                 dicoms.append(os.path.join(root, file))
+    dicoms = sorted(dicoms)
+    arrs = []
+    for dicom_path in dicoms:
+        ds = dcmread(dicom_path)
+        arr = ds.pixel_array
+        arrs.append(arr)
+    arr = np.array(arrs)
+    axis = 1
+    img = np.max(arr, axis=axis)
+    img = normalize(img)
+    img = resize(img, OUTPUT_DIM)
+    skimage.io.imsave(f'{output_dir}/{idx:04}.png', (img * 255).astype(np.uint8))
+
+
 def ct():
+    ricord_root = f'{RICORD_DIR}/MIDRC-RICORD-1B'
+    ricord_patient_dirs = [os.path.join(ricord_root, d) for d in os.listdir(ricord_root) if os.path.isdir(os.path.join(ricord_root, d))]
+
+    os.makedirs(OUTPUT_B_TRAIN, exist_ok=True)
+    for idx in range(N_TRAIN):
+        process_one_ct(ricord_patient_dirs[idx], OUTPUT_B_TRAIN, idx)
+
+    os.makedirs(OUTPUT_B_VAL, exist_ok=True)
+    for idx in range(N_VAL):
+        process_one_ct(ricord_patient_dirs[N_TRAIN + idx], OUTPUT_B_VAL, idx)
+
+    os.makedirs(OUTPUT_B_TEST, exist_ok=True)
+    for idx in range(N_TEST):
+        process_one_ct(ricord_patient_dirs[N_TRAIN + N_VAL + idx], OUTPUT_B_TEST, idx)
 
 
-#xray()
+xray()
 ct()
